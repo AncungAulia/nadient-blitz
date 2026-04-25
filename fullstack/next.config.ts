f37bcbdc9ba7e5478fb093e81ea49991ea2496a7
ancung <ancungaulia@gmail.com>
2026-04-25 17:30:36 +0700

    feat(fe): fix next config

--- fullstack/next.config.ts
+++ fullstack/next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  turbopack: {
+    root: process.cwd(),
+  },
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "wagmi",
+      "viem",
+      "@radix-ui/react-tabs",
+      "@radix-ui/react-tooltip",
+      "@radix-ui/react-slider",
+      "@radix-ui/react-switch",
+    ],
+  },
 };
 
 export default nextConfig;
